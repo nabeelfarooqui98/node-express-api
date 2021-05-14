1665672cf0bdec5de46e044361e4dbbad0a051ba
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2021-05-14 05:07:38 +0500

    post request

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,28 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+];
+
 router.get('/', (req, res) => {
-    res.send('Users get');
+    console.log(users);
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(`User with name ${user.firstName} added`);
 });
 
 

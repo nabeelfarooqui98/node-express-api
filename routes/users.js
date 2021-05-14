34e4ed69cdb6bc06144fc7cbb717fd56860e70d2
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2021-05-14 05:13:22 +0500

    uuid usage

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
@@ -22,8 +24,11 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const user = req.body;
-    users.push(user);
-    res.send(`User with name ${user.firstName} added`);
+
+    const userWithId = { ...user, id: uuidv4() };
+
+    users.push(userWithId);
+    res.send(`User with name ${userWithId.firstName} added`);
 });
 
 
